771319e6f7f4bbf65c47a28ee8fc2b1794c4c67b
BuildTools <santipego0001@gmail.com>
2020-09-02 09:07:13 -0500

    Resuelto problema número 2

--- ProblemaNumeroPar.py
+++ ProblemaNumeroPar.py
@@ -4,4 +4,14 @@ Created on Wed Sep  2 08:56:37 2020
 
 @author: Santiago Pérez
 """
-
+continuar=True
+while continuar:
+    try:
+       numero=int(input("Digite un número (entero) para saber si es par o no: "))
+       if numero%2==0:
+           print("Número par")
+       else:
+           print("Número impar") 
+       continuar=False
+    except ValueError:
+        print("El valor digitado debe ser un número entero")

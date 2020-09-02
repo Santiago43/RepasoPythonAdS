e7b87dff7fdeeedeab7026660fe9b2edd7beaae3
BuildTools <santipego0001@gmail.com>
2020-09-01 22:35:04 -0500

    Primer intento de problema 1

--- Problema2070.py
+++ Problema2070.py
@@ -5,3 +5,14 @@ Created on Tue Sep  1 22:16:25 2020
 @author: Santiago Pérez
 """
 
+edad=int(input("Digite su edad: "))
+if (edad<0):
+    #exit("La edad no puede ser negativa")
+    print("Edad no puede ser negativa\n")
+añoActual=int(input("Digite el año actual: "))
+if (añoActual<0):
+    print("Año no puede ser negativo\n")
+    #exit("El año no puede ser negativo")
+edadFutura=int((2070-añoActual)+edad)
+print("Su edad futura es de: ")
+print(edadFutura)

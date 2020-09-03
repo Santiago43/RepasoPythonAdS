24ef8af6fd91a3f88ccff53f940bc930359bd0e3
BuildTools <santipego0001@gmail.com>
2020-09-03 12:49:56 -0500

    Se agrega prueba unitaria al problema 1

--- Problema2070.py
+++ Problema2070.py
@@ -5,21 +5,4 @@ Created on Tue Sep  1 22:16:25 2020
 @author: Santiago Pérez
 """
 def edadFutura(edad,año):
-    return (2070-añoActual)+edad
-continuar = True
-while (continuar):
-    try:
-        edad=int(input("Digite su edad: "))
-        if (edad<0):
-            raise Exception("Su edad no puede ser negativa")
-        añoActual=int(input("Digite el año actual: "))
-        if (añoActual<0):
-            raise Exception("El año no puede ser negativo")
-        elif (añoActual>2070):
-            raise Exception("La idea es que digite un año inferior al 2070")
-        edadFutura=edadFutura(edad,añoActual)
-        print("Su edad futura es de: "+str(edadFutura)+" años")
-        continuar=False
-    except Exception as e:
-        print(e)
-
+    return (2070-año)+edad

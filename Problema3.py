081fc31a132964322eaac417b4d4160bfa378260
BuildTools <santipego0001@gmail.com>
2020-09-03 13:04:26 -0500

    Se eliminan comentarios de Problema 3

--- Problema3.py
+++ Problema3.py
@@ -5,6 +5,4 @@ Created on Wed Sep  2 08:35:59 2020
 @author: Santiago Pérez
 """
 def primeroYUltimo(palabra):
-    #print("El primer caracter es: "+palabra[0])
-    #print("El último caracter es: "+palabra[-1])
     return palabra[0]+palabra[-1]

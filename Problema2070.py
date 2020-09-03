5c10c8ecd8c1760fcc043c3bdf7872a28085e731
BuildTools <santipego0001@gmail.com>
2020-09-03 12:45:06 -0500

    Corregido problema 1

--- Problema2070.py
+++ Problema2070.py
@@ -4,6 +4,8 @@ Created on Tue Sep  1 22:16:25 2020
 
 @author: Santiago Pérez
 """
+def edadFutura(edad,año):
+    return (2070-añoActual)+edad
 continuar = True
 while (continuar):
     try:
@@ -15,7 +17,7 @@ while (continuar):
             raise Exception("El año no puede ser negativo")
         elif (añoActual>2070):
             raise Exception("La idea es que digite un año inferior al 2070")
-        edadFutura=int((2070-añoActual)+edad)
+        edadFutura=edadFutura(edad,añoActual)
         print("Su edad futura es de: "+str(edadFutura)+" años")
         continuar=False
     except Exception as e:

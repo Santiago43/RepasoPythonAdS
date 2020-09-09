78008d824661792c8e5823b6836c3bba5124386f
BuildTools <santipego0001@gmail.com>
2020-09-09 10:45:45 -0500

    Agregado Problema3 y su test correctamente

--- Problema3.py
+++ Problema3.py
@@ -4,7 +4,7 @@ Created on Wed Sep  2 08:35:59 2020
 
 @author: Santiago Pérez
 """
-def primeroYUltimo(palabra):
-    #print("El primer caracter es: "+palabra[0])
-    #print("El último caracter es: "+palabra[-1])
-    return palabra[0]+palabra[-1]
+def primeraLetra(palabra):
+    return palabra[0]
+def últimaLetra(palabra):
+    return palabra[-1]

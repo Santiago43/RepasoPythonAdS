38f9675be95670cb3aa693a191e7259c3ab35dab
BuildTools <santipego0001@gmail.com>
2020-09-17 12:00:21 -0500

    Se agregan cambios a clase punto y se crea prueba unitaria de punto

--- Problema Geometría/Punto.py
+++ Problema Geometría/Punto.py
@@ -6,7 +6,7 @@ class Punto:
         self.y = y
 
     def calcularDistancia(self, otroPunto):
-        return ((self.x-otroPunto.x)**2-(self.y-otroPunto.y)**2)**(1/2)
+        return ((self.x-otroPunto.x)**2+(self.y-otroPunto.y)**2)**(1/2)
 
     def determinarDentro(self, circulo):
         return self.calcularDistancia(circulo.puntos[0])<=circulo.radio
---
+++ Problema Geometría/TestPunto.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 17 11:55:43 2020
+
+@author: Santiago Pérez
+"""
+from Punto import Punto
+import unittest
+
+class TestPunto(unittest.TestCase):
+
+    def test_distancia(self):
+        punto1= Punto(0,0)
+        punto2= Punto(3,4)
+        
+        self.assertEqual(5,punto1.calcularDistancia(punto2))
+
+if __name__ == '__main__':
+    unittest.main()
